d0c6b33171956c0f190b52be0adbf745b76bb60c
Qingyun Li (from Modal) <qingy2019@outlook.com>
2026-02-13 07:53:43 +0000

    update simpledeco command

--- script/autodeco_eval.sh
+++ script/autodeco_eval.sh
@@ -9,21 +9,21 @@
 #   --tp_size 1
 
 # --- AutoDeco ---
+# python utils/llm_eval.py \
+#   --model_name_or_path ckpt/autodeco-qwen3-4b-thinking \
+#   --dataset aime24 \
+#   --temp 1.0 \
+#   --top_p 0.95 \
+#   --k 16 \
+#   --seed 42 \
+#   --tp_size 1
+
+# --- SimpleDeco ---
 python utils/llm_eval.py \
-  --model_name_or_path ckpt/autodeco-qwen3-4b-thinking \
+  --model_name_or_path ckpt/simpledeco-qwen3-4b-thinking-merged \
   --dataset aime24 \
   --temp 1.0 \
   --top_p 0.95 \
   --k 16 \
-  --seed 42 \
+  --seed 43 \
   --tp_size 1
-
-# --- SimpleDeco ---
-# python utils/llm_eval.py \
-#   --model_name_or_path ckpt/autodeco-qwen3-4b-thinking-merged \
-#   --dataset aime24 \
-#   --temp 1.0 \
-#   --top_p 0.99 \
-#   --k 16 \
-#   --seed 42 \
-#   --tp_size 1
