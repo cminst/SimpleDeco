00bbdc735913834cbca800966544687140c0b28a
Qingyun Li (from Modal) <qingy2019@outlook.com>
2026-02-13 05:58:05 +0000

    simpledeco command

--- script/autodeco_eval.sh
+++ script/autodeco_eval.sh
@@ -1,4 +1,4 @@
-# Base model
+# --- Base model ---
 # python utils/llm_eval.py \
 #   --model_name_or_path ckpt/qwen3-4b-thinking \
 #   --dataset aime24 \
@@ -8,6 +8,7 @@
 #   --seed 42 \
 #   --tp_size 1
 
+# --- AutoDeco ---
 python utils/llm_eval.py \
   --model_name_or_path ckpt/autodeco-qwen3-4b-thinking \
   --dataset aime24 \
@@ -16,3 +17,13 @@ python utils/llm_eval.py \
   --k 16 \
   --seed 42 \
   --tp_size 1
+
+# --- SimpleDeco ---
+# python utils/llm_eval.py \
+#   --model_name_or_path ckpt/autodeco-qwen3-4b-thinking-merged \
+#   --dataset aime24 \
+#   --temp 1.0 \
+#   --top_p 0.99 \
+#   --k 16 \
+#   --seed 42 \
+#   --tp_size 1
