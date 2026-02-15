2a2bbac87b841291794b6210bf4920ab4d524f74
Qingyun Li (from Modal) <qingy2019@outlook.com>
2026-02-15 03:17:19 +0000

    added the top-p free command

--- script/autodeco_eval.sh
+++ script/autodeco_eval.sh
@@ -19,11 +19,23 @@
 #   --tp_size 1
 
 # --- SimpleDeco ---
+# python utils/llm_eval.py \
+#   --model_name_or_path ckpt/simpledeco-qwen3-4b-thinking-merged \
+#   --dataset aime24 \
+#   --temp 1.0 \
+#   --top_p 0.95 \
+#   --k 16 \
+#   --seed 43 \
+#   --tp_size 4
+
+# --- No Top-P Guardrails ---
 python utils/llm_eval.py \
   --model_name_or_path ckpt/simpledeco-qwen3-4b-thinking-merged \
   --dataset aime24 \
   --temp 1.0 \
-  --top_p 0.95 \
+  --top_p 1.0 \
+  --top_k -1 \
   --k 16 \
-  --seed 43 \
-  --tp_size 1
+  --mode maj@k \
+  --seed 42 \
+  --tp_size 1  2>&1 | tee ckpt/simpledeco_aime24_handsoff.log
