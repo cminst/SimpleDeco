d56f001ec2523d730fb0fd5505db27f507ee88f5
Qingyun Li <qingy2019@outlook.com>
2026-02-28 15:09:47 -0800

    new training comman

--- script/simpledeco_train.sh
+++ script/simpledeco_train.sh
@@ -1,20 +1,21 @@
 export WANDB_PROJECT="ESAD"
 TS=$(date +%Y%m%d%H%M%S)
 
-export WANDB_RUN_ID="SimpleDeco-NewV2-$TS"
+export WANDB_RUN_ID="SimpleDeco-V3-$TS"
 
 uv run accelerate launch trl_train.py \
   --model_name_or_path ckpt/untrained-autodeco-qwen3-4b-thinking \
   --dataset_name qingy2024/Dolci-Think-SFT-ctx8k \
-  --output_dir ./ckpt/simpledeco-bimodal \
+  --output_dir ./ckpt/simpledeco-bimodal-uniform \
   --train_temp true \
   --train_top_p false \
   --temp_objective analytic_min_p_hinge \
   --min_p_ratio 0.05 \
-  --temp_hinge_weight 3.0 \
-  --temp_reg_weight 1.5 \
-  --goldilocks_temp_cap 0.9 \
-  --temp_target_smooth_window 32 \
+  --temp_hinge_weight 1.0 \
+  --temp_reg_weight 0.2 \
+  --goldilocks_temp_cap 1.0 \
+  --temp_target_smooth_window 2 \
+  --goldilocks_uniform \
   --max_steps 3000 \
   --logging_steps 25 \
   --per_device_train_batch_size 16 \
@@ -25,7 +26,7 @@ uv run accelerate launch trl_train.py \
   --assistant_only_loss true \
   --temp_diag_enabled true \
   --temp_diag_steps 100 \
-  --temp_diag_examples 3 \
+  --temp_diag_examples 10 \
   --temp_diag_topk 10 \
   --report_to wandb \
   --num_proc 16
