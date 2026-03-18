a99e5632df6047e107dbc8b1b0baff147e886e68
Qingyun Li <qingy2019@outlook.com>
2026-03-18 12:32:15 -0700

    better vllm install script

--- utils/install_simpledeco_vllm.sh
+++ utils/install_simpledeco_vllm.sh
@@ -1,3 +1,3 @@
 cd simpledeco_vllm
-VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/0.10.2/vllm-0.10.2-cp38-abi3-manylinux1_x86_64.whl UV_CONCURRENCY=24 uv pip install -U -e . --no-build-isolation && uv pip install transformers==4.56.0 trl==0.22.0
+VLLM_USE_PRECOMPILED=1 UV_CONCURRENCY=24 uv pip install -U -e . --no-build-isolation && uv pip install transformers==4.56.0 trl==0.22.0
 cd ..
