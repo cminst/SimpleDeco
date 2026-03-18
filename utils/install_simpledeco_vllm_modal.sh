373bfe2a785cd5d037a3b5c1406c23e05346459a
Qingyun Li <qingy2019@outlook.com>
2026-03-18 12:34:21 -0700

    update modal

--- utils/install_simpledeco_vllm_modal.sh
+++ utils/install_simpledeco_vllm_modal.sh
@@ -1,5 +1,5 @@
 cd simpledeco_vllm
-VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/0.10.2/vllm-0.10.2-cp38-abi3-manylinux1_x86_64.whl \
+VLLM_USE_PRECOMPILED=1 \
     uv pip install -U -e . --no-build-isolation --system --torch-backend=cu128 \
     && uv pip install transformers==4.56.0 trl==0.22.0 --system --torch-backend=cu128
 cd ..
