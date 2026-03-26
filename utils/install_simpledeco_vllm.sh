6cdb7e591ee19ffdbf5f73035fd5dc825674c492
Qingyun Li <qingy2019@outlook.com>
2026-03-25 21:07:10 -0700

    updates

--- utils/install_simpledeco_vllm.sh
+++ utils/install_simpledeco_vllm.sh
@@ -1,3 +1,3 @@
 cd simpledeco_vllm
-VLLM_USE_PRECOMPILED=1 UV_CONCURRENCY=24 uv pip install -U -e . --no-build-isolation && uv pip install transformers==4.56.0 trl==0.22.0
+VLLM_USE_PRECOMPILED=1 VLLM_PRECOMPILED_WHEEL_LOCATION="https://wheels.vllm.ai/c9d838fc338db9a5a23cb3906d17c47423c4c9e4/vllm-0.17.2rc1.dev71%2Bgc9d838fc3-cp38-abi3-manylinux_2_31_x86_64.whl" UV_CONCURRENCY=24 uv pip install -U -e . --no-build-isolation && uv pip install transformers==4.56.0 trl==0.22.0
 cd ..
