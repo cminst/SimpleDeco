3eda1a6a92e1e7c3d12bbd972e9015630dd7a30c
Qingyun Li <qingy2019@outlook.com>
2026-03-25 20:47:08 -0700

    vllm

--- utils/install_simpledeco_vllm_modal.sh
+++ utils/install_simpledeco_vllm_modal.sh
@@ -1,5 +1,5 @@
 cd simpledeco_vllm
-VLLM_USE_PRECOMPILED=1 \
+VLLM_PRECOMPILED_WHEEL_LOCATION="https://wheels.vllm.ai/95c0f928cdeeaa21c4906e73cee6a156e1b3b995/vllm-0.17.1-cp311-abi3-manylinux_2_31_x86_64.whl" \
     uv pip install -U -e . --no-build-isolation --system --torch-backend=cu128 \
     && uv pip install transformers==4.56.0 trl==0.22.0 --system --torch-backend=cu128
 cd ..
--- utils/modal_gpu.py
+++ utils/modal_gpu.py
@@ -23,7 +23,7 @@ image = (
         'git config --global user.name "Qingyun Li (from Modal)"',
         'git config --global credential.helper store',
     )
-    .run_commands("echo 'VERSION 5'")
+    .run_commands("echo 'VERSION 6'")
     .run_commands(
         "uv pip install -U torchvision torchaudio torch==2.10.0 'numpy<=2.2' vllm==0.17.1 triton==3.6.0 --torch-backend=cu128 --no-build-isolation --system",
         "uv pip install accelerate trl==0.22.0 deepspeed orjson hf_transfer setuptools_scm --torch-backend=cu128 --system",
